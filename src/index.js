6fd2573a3ff42bbf30e751b8f1a8e46c3da8db20
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2023-07-23 15:49:51 -0500

    Adds Database Connection
    This adds a class for connecting and querying the database. This will allow us to get the data from the database that we need to provide in our API endpoints.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import express from 'express';
+import { Database } from './db/connection.js';
 import Routes from './routes.js';
 
 dotenv.config()
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000
 
 function startApp() {
     const app = express()
+    Database.startConnection()
 
     Routes.apply(app)
     
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,25 @@
+import { Database } from "./db/connection.js"
+
 export default class Routes {
     static apply(app) {
-        app.get('/test', (req, res) => {
-            res.send("Hello World!")
+        app.get('/items', async (req, res) => {
+            const result = await new Promise((resolve, reject) => {
+                Database.getConnection().query('SELECT * FROM items', (err, data) => {
+                    if (err) reject(err)
+                    resolve(data)
+                })
+            })
+            res.send(result)
+        })
+
+        app.get('/categories', async (req, res) => {
+            const result = await new Promise((resolve, reject) => {
+                Database.getConnection().query('SELECT * FROM category', (err, data) => {
+                    if (err) reject(err)
+                    resolve(data)
+                })
+            })
+            res.send(result)
         })
     }
 }
