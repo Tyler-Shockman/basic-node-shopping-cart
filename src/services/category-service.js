7b6418113cec9286c48999618029b0b468d6a410
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2023-07-26 22:22:00 -0500

    Added Some TODO Blocks for Changes I Want to Make
    Added a couple different TODO comments for changes I know I want to make but don't want to forget.

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -14,6 +14,8 @@ class CategoryService {
 
     async getCategory(categoryId) {
         const category = (await new Promise((resolve, reject) => {
+            // TODO: REMOVE THE ${categoryId} FROM THE QUERY STRING AND PUT INTO A QUERY PARAM. THIS WILL HELP PROTECT AGAINST SQL INJECTION.
+            // TODO: SELECT EXACTLY WHAT IS NEEDED HERE. THIS PROTECTS AGAINST SOMETHING NEED AND CONFIDENTIAL GETTING ADDED IN THE FUTURE AND THEN RETURNED. IT IS ALSO A GOOD OVERALL PRACTICE.
             database.getConnection().query(`SELECT * FROM category WHERE id = ${categoryId}`, (err, data) => {
                 if (err) reject(err)
                 resolve(data)
--- src/services/item-service.js
+++ src/services/item-service.js
@@ -2,8 +2,11 @@ import { database } from "../db/connection.js";
 import ItemNotFoundException from "../exceptions/ItemNotFoundException.js";
 
 export default class ItemService {
+    // TODO: RE-ORDER THE METHODS HERE IN THE SAME ORDER AS IN category-service.js.
     async getItem(itemId) {
         const item = (await new Promise((resolve, reject) => {
+            // TODO: REMOVE THE ${itemId} FROM THE QUERY STRING AND PUT INTO A QUERY PARAM. THIS WILL HELP PROTECT AGAINST SQL INJECTION.
+            // TODO: SELECT EXACTLY WHAT IS NEEDED HERE. THIS PROTECTS AGAINST SOMETHING NEED AND CONFIDENTIAL GETTING ADDED IN THE FUTURE AND THEN RETURNED. IT IS ALSO A GOOD OVERALL PRACTICE.
             database.getConnection().query(`SELECT * FROM items WHERE id = ${itemId}`, (err, data) => {
                 if (err) reject(err)
                 resolve(data)
