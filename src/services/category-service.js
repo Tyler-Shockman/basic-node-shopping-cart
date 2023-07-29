6243036e883184f6de55bfb26ef36b0f0c621173
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2023-07-28 21:06:26 -0500

    Adjusted Queries in Category Service
    Adjusted the queries in the category service to improve security. This includes selecting exact columns to reduce risk of returning unintended values and passing in data via parameter to protect against SQL injection.

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -4,8 +4,7 @@ import CategoryNotFoundException from "../exceptions/CategoryNotFoundException.j
 class CategoryService {
     async getAllCategories() {
         const categories = await new Promise((resolve, reject) => {
-            // TODO: SELECT EXACTLY WHAT IS NEEDED.
-            database.getConnection().query('SELECT * FROM category', (err, data) => {
+            database.getConnection().query('SELECT id, name FROM category', (err, data) => {
                 if (err) reject(err)
                 resolve(data)
             })
@@ -15,9 +14,7 @@ class CategoryService {
 
     async getCategory(categoryId) {
         const category = (await new Promise((resolve, reject) => {
-            // TODO: REMOVE THE ${categoryId} FROM THE QUERY STRING AND PUT INTO A QUERY PARAM. THIS WILL HELP PROTECT AGAINST SQL INJECTION.
-            // TODO: SELECT EXACTLY WHAT IS NEEDED HERE. THIS PROTECTS AGAINST SOMETHING NEED AND CONFIDENTIAL GETTING ADDED IN THE FUTURE AND THEN RETURNED. IT IS ALSO A GOOD OVERALL PRACTICE.
-            database.getConnection().query(`SELECT * FROM category WHERE id = ${categoryId}`, (err, data) => {
+            database.getConnection().query(`SELECT id, name FROM category WHERE id = ?`, [categoryId], (err, data) => {
                 if (err) reject(err)
                 resolve(data)
             })
