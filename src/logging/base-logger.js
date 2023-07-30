9f41decd17b8aa4b4fd017fd59b7f38fd9211b91
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2023-07-30 16:33:16 -0500

    Added Logger with Basic Methods
    Added a new custom logger with basic methods for the different log actions. This can replace our calls to console.log (etc.) in the future.

---
+++ src/logging/base-logger.js
@@ -0,0 +1,35 @@
+export default class BaseLogger {
+    log(message) {
+        console.log(message)
+    }
+
+    critical(message) {
+        console.error(message)
+    }
+
+    error(message) {
+        console.error(message)
+    }
+
+    warn(message) {
+        console.warn(message)
+    }
+
+    notice(message) {
+        console.notice(message)
+    }
+
+    info(message) {
+        console.info(message)
+    }
+
+    debug(message) {
+        console.debug(message)
+    }
+
+    trace(message) {
+        console.trace(message)
+    }
+}
+
+const baseLogger = new BaseLogger()
